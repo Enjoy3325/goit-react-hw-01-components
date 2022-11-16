f463168823281b9e36f6e0e55b74e6c02352f035
Enjoy3325 <enjoy3325@gmail.com>
2022-11-17 01:07:06 +0200

    normalize add

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,7 +3,6 @@ import { Status } from './FriendListItem.styled';
 export function FriendListItem({ avatar, name, isOnline }) {
   return (
     <Item>
-      {' '}
       <Status statusType={isOnline}>{isOnline}</Status>
       <img className="avatar" src={avatar} alt={name} width="70" />
       <p className="name">{name}</p>
